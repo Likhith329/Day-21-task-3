af316a7affde8beb93f74fd0ad3969b7ea6e0691
Likhith kumar <likhithmattapalli@gmail.com>
2022-10-27 21:41:34 +0530

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ let solutiondata=document.querySelector(".solution")
 btnsdata.forEach(btndata=>{
     btndata.addEventListener("click",()=>{
         let inpdata=document.querySelector("input[type='text']").value
-        getData(btndata.value,inpdata)
+        if(inpdata!=''){
+            getData(btndata.value,inpdata)
+        }
+        else{
+            alert("Enter any expression or equation!")
+        }
     })
 })
 
@@ -15,7 +20,6 @@ async function getData(operation,inputdata){
     let result=document.querySelector(".solution")
     result.innerHTML="Answer is "+data.result
 }
-
 
 function show(className){
     let displaydata=document.querySelector("."+className)
@@ -29,30 +33,22 @@ function hide(className){
 async function tangent(){
     let xval=document.querySelector(".bc1").value
     let eqval=document.querySelector(".eq1").value
-    let inpdata=encodeURIComponent(xval+"|"+eqval)
-    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/tangent/"+inpdata)
-    let data= await fetcheddata.json()
-    let result=document.querySelector(".solution")
-    result.innerHTML="Answer is "+data.result
+    let inpdata=xval+"|"+eqval
+   getData("tangent",inpdata)
 }
-
 
 async function Area(){
     let x1val=document.querySelector(".bcon1").value
     let x2val=document.querySelector(".bcon2").value
     let eqval=document.querySelector(".eq2").value
-    let inpdata=encodeURIComponent(x1val+":"+x2val+"|"+eqval)
-    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/area/"+inpdata)
-    let data= await fetcheddata.json()
-    let result=document.querySelector(".solution")
-    result.innerHTML="Answer is "+data.result
+    let inpdata=x1val+":"+x2val+"|"+eqval
+    getData("area",inpdata)
 }
-
 
 async function Log(){
     let baseval=document.querySelector(".base").value
     let argval=document.querySelector(".argument").value
-    let inpdata=encodeURIComponent(baseval+"|"+argval)
+    let inpdata=baseval+"|"+argval
     let fetcheddata= await fetch("https://newton.vercel.app/api/v2/log/"+inpdata)
     let data= await fetcheddata.json()
     let result=document.querySelector(".solution")
