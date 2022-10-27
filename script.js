2823ac8613c732ffedb5d22426e5d5ecc510a8dc
Likhith kumar <likhithmattapalli@gmail.com>
2022-10-27 21:31:06 +0530

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-let bodydata=document.querySelector(".tbody")
-let row=''
+let btnsdata=document.querySelectorAll(".btn.btn-warning")
+let solutiondata=document.querySelector(".solution")
+btnsdata.forEach(btndata=>{
+    btndata.addEventListener("click",()=>{
+        let inpdata=document.querySelector("input[type='text']").value
+        getData(btndata.value,inpdata)
+    })
+})
 
-async function getData(){
-let fetched=await fetch("https://kontests.net/api/v1/all")
-let data=await fetched.json();
-let a=0
-data.forEach(element => {
-    row+=`
-    <tr>
-    <td>${++a}</td>
-    <td class='event'>${element.name}</td>
-    <td>${element.start_time.slice(0,10)} at ${element.start_time.slice(11,16)}</td>
-    <td>${element.end_time.slice(0,10)} at ${element.end_time.slice(11,16)}</td>
-    <td>${(element.duration/3600).toFixed(2)} Hours</td>
-    <td><a href="${element.url}" target='_blank'><button class='btn btn-primary'>${element.site}</button></a></td>
-    </tr>
-    `
-});
-bodydata.innerHTML=row
+async function getData(operation,inputdata){
+    inputdata=encodeURIComponent(inputdata)
+
+    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/"+operation+"/"+inputdata)
+    let data= await fetcheddata.json()
+    let result=document.querySelector(".solution")
+    result.innerHTML="Answer is "+data.result
 }
 
-getData();
+
+function show(className){
+    let displaydata=document.querySelector("."+className)
+    displaydata.style.display="block"
+}
+function hide(className){
+    let displaydata=document.querySelector("."+className)
+    displaydata.style.display="none"
+}
+
+async function tangent(){
+    let xval=document.querySelector(".bc1").value
+    let eqval=document.querySelector(".eq1").value
+    let inpdata=encodeURIComponent(xval+"|"+eqval)
+    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/tangent/"+inpdata)
+    let data= await fetcheddata.json()
+    let result=document.querySelector(".solution")
+    result.innerHTML="Answer is "+data.result
+}
+
+
+async function Area(){
+    let x1val=document.querySelector(".bcon1").value
+    let x2val=document.querySelector(".bcon2").value
+    let eqval=document.querySelector(".eq2").value
+    let inpdata=encodeURIComponent(x1val+":"+x2val+"|"+eqval)
+    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/area/"+inpdata)
+    let data= await fetcheddata.json()
+    let result=document.querySelector(".solution")
+    result.innerHTML="Answer is "+data.result
+}
+
+
+async function Log(){
+    let baseval=document.querySelector(".base").value
+    let argval=document.querySelector(".argument").value
+    let inpdata=encodeURIComponent(baseval+"|"+argval)
+    let fetcheddata= await fetch("https://newton.vercel.app/api/v2/log/"+inpdata)
+    let data= await fetcheddata.json()
+    let result=document.querySelector(".solution")
+    result.innerHTML="Answer is "+data.result+` ( log<sub>${baseval}</sub>${argval} )`
+}
+
